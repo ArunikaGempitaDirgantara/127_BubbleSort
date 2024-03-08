7894cea812ef499875c953b955089d19444625cf
ArunikaGempitaDirgantara <iqbalsyabanaazmi81@gmail.com>
2024-03-08 11:07:38 +0700

    Menambahkan Fungsi atau Prosedur Input

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -6,8 +6,26 @@ using namespace std;
 
 int a[20]; //Deklarasi array a dengan ukuran 20
 int n; //Deklarasi variabel n untuk menyimpan banyaknya elemen pada array
-{
 
+void input() { //procedur untuk input
+	while (true) { //looping
+		cout << "Masukkan banyaknya elemen pada array : "; //Output ke layar
+		cin >> n; //Input dari pengguna
+		if (n <= 20) //Jika n kurang dari sama dengan 20
+			break; //Keluar dari loop
+		else { //Jika n lebih dari 20
+			cout << "\nArray dapat mempunyai maksimal 20 elemen. \n"; //Output ke layar
+		}
+	}
+	cout << endl; //Output baris kosong
+	cout << "==========" << endl; //Output ke layar
+	cout << "Masukkan elemen array" << endl; //Output ke layar
+	cout << "==========" << endl; //Output ke layar
+
+	for (int i = 0; i < n; i++) { //Looping dengan i dimulai dari 0 hingga n-1
+		cout << "Data ke-" << (i + 1) << ": "; //Output ke layar
+		cin >> a[i]; //Input dari pengguna
+	}
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
