7fda03ec9242e2ebd62faaf3fdddd3fc8af92ff4
ArunikaGempitaDirgantara <iqbalsyabanaazmi81@gmail.com>
2024-03-08 11:15:05 +0700

    Menambahkan Fungsi atau Prosedur BubbleSort

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -28,6 +28,24 @@ void input() { //procedur untuk input
 	}
 }
 
+void bubbleSortArray() { //procedur untuk mengurutkan array dengan metode bubble sort
+	int pass = 1; //step 1
+	do {
+		for (int j = 0; j <= n - 1 - pass; j++) { //step 2
+			if (a[j] > a[j + 1]) { //step 3
+				int temp;
+				temp = a[j];
+				a[j] = a[j + 1];
+				a[j + 1] = temp;
+			}
+		}
+		pass = pass + 1; //step 4
+	} while (pass <= n - 1); //step 5
+	{
+
+	}
+}
+
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
 
