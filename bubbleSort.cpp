9fcfe1d521e7fa0a4c028202efcb2f3480539e54
ArunikaGempitaDirgantara <iqbalsyabanaazmi81@gmail.com>
2024-03-08 11:25:20 +0700

    Menambahkan Fungsi atau Prosedur Main

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -55,6 +55,16 @@ void display() {
 	cout << endl;
 }
 
+int main() {
+
+	input(); //Memanggil procedur input()
+	bubbleSortArray(); //Mengurutkan data dengan algoritma bubble sort
+	display(); //Memanggil procedur display()
+	system("pause");
+
+	return 0;
+}
+
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
 
