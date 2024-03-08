ee0640725a01b688a6fc25937cf1487f1e4b034e
ArunikaGempitaDirgantara <iqbalsyabanaazmi81@gmail.com>
2024-03-08 10:51:23 +0700

    Commit Variabel Global

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -2,8 +2,10 @@
 //
 
 #include <iostream>
+using namespace std;
 
-int main()
+int a[20]; //Deklarasi array a dengan ukuran 20
+int n; //Deklarasi variabel n untuk menyimpan banyaknya elemen pada array
 {
 
 }
